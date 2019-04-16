aefd744a934138cbef77f94ed6cc51aa6447fa09
Jimmy <jimmy.gense@lacatholille.fr>
2019-04-16 20:54:56 +0200

    Fin squelette

--- src/app/clan/clan.module.ts
+++ src/app/clan/clan.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClanComponent } from './clan.component';
 import {CustomMaterialModule} from '../custom-material.module';
+import {SharedModule} from '../utilities/shared.module';
 
 @NgModule({
   declarations: [ClanComponent],
   imports: [
     CommonModule,
     CustomMaterialModule,
+    SharedModule,
   ]
 })
 export class ClanModule { }
